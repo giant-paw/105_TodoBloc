3bdd0eb163f157cab82eac1395d182e0792cd8fc
giant-paw <giantpawbusiness@gmail.com>
2025-05-08 17:55:26 +0700

    Menambahkan BlocProvider untuk TodoBloc => main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:tugas_bloc/bloc/todo_bloc.dart';
 import 'package:tugas_bloc/todo_page.dart';
 
 void main() {
@@ -10,12 +12,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return BlocProvider(
+      create: (context) => TodoBloc(),
+      child: MaterialApp(
+        title: 'Tugas Todo Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+
+        home: TodoPage(),
+        debugShowCheckedModeBanner: false,
       ),
-      home: TodoPage()
     );
   }
 }
