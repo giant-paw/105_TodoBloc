331701ecea65878916cb06ba047fe74e8161d2c0
giant-paw <giantpawbusiness@gmail.com>
2025-05-08 20:43:27 +0700

    Menambahkan tombol pilih tanggal dengan date picker

--- lib/todo_page.dart
+++ lib/todo_page.dart
@@ -43,6 +43,27 @@ class _TodoPageState extends State<TodoPage> {
                       ),
                     ],
                   ),
+
+                  SizedBox(width: 16.0),
+                  Expanded(
+                    child: ElevatedButton(
+                      onPressed: () {
+                        showDatePicker(
+                          context: context,
+                          initialDate: DateTime.now(),
+                          firstDate: DateTime(2000),
+                          lastDate: DateTime(2100),
+                        ).then((selectedDate) {
+                          if (selectedDate != null) {
+                            context.read<TodoBloc>().add(
+                              TodoSelectDate(date: selectedDate),
+                            );
+                          }
+                        });
+                      },
+                      child: Text('Pilih Tanggal'),
+                    ),
+                  ),
                 ],
               ),
             ],
